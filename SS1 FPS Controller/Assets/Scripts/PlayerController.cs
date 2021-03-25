c6485a2b26846802aee7a4488c3ea068d5a91347
Jack Hill <jackhill2104@gmail.com>
2021-03-26 00:46:18 +1030

    asdfjknaskjfn

--- SS1 FPS Controller/Assets/Scripts/PlayerController.cs
+++ SS1 FPS Controller/Assets/Scripts/PlayerController.cs
@@ -2,10 +2,15 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] float snappiness = 100f;
+    [SerializeField] float snappiness = 10f;
 
     private float xAccumulator;
     private float yAccumulator;
+
+    private void Start()
+    {
+        Application.targetFrameRate = 60;
+    }
 
     // Update is called once per frame
     void Update()
@@ -14,15 +19,16 @@ public class PlayerController : MonoBehaviour
         float mouseY = Input.GetAxis("Mouse Y");
 
         // Linearly interpolate from xAccumulator/yAccumulator to mouseX/mouseY
-        xAccumulator = Mathf.Lerp(xAccumulator, mouseX, snappiness);
-        yAccumulator += Mathf.Lerp(xAccumulator, mouseY, snappiness);
+        // Time.deltaTime makes the camera movement frame-rate independent
+        xAccumulator = Mathf.Lerp(xAccumulator, mouseX, snappiness * Time.deltaTime);
+        yAccumulator = Mathf.Lerp(yAccumulator, mouseY, snappiness * Time.deltaTime);
 
         transform.Rotate(0, xAccumulator, 0, Space.World);
+        Camera.main.transform.Rotate(-yAccumulator, 0, 0);
 
-        // Clamp vertical angle between -85 and 85 degrees. The rotation is directly set
-        // as opposed to using Rotate() to allow for easy clamping
-        yAccumulator = Mathf.Clamp(yAccumulator, -85, 85);
-        Camera.main.transform.localEulerAngles = new Vector3(-yAccumulator, 0, 0);
+        // Clamp vertical angle between -90 and 90 degrees
+        float verticalAngle = Mathf.Clamp(Camera.main.transform.localEulerAngles.x, 0, 360);
+        Camera.main.transform.localEulerAngles = new Vector3(verticalAngle, 0, 0);
     }
 }
 
