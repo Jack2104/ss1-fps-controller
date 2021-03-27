4fd070402dababf668f24575a9fab741c841b43a
Jack Hill <jackhill2104@gmail.com>
2021-03-28 02:36:10 +1030

    Update PlayerController.cs

--- SS1 FPS Controller/Assets/Scripts/PlayerController.cs
+++ SS1 FPS Controller/Assets/Scripts/PlayerController.cs
@@ -15,6 +15,8 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float currentVerticalAngle = Mathf.Abs(Camera.main.transform.localEulerAngles.x);
+
         float mouseX = Input.GetAxis("Mouse X");
         float mouseY = Input.GetAxis("Mouse Y");
 
@@ -27,8 +29,15 @@ public class PlayerController : MonoBehaviour
         Camera.main.transform.Rotate(-yAccumulator, 0, 0);
 
         // Clamp vertical angle between -90 and 90 degrees
-        float verticalAngle = Mathf.Clamp(Camera.main.transform.localEulerAngles.x, 0, 360);
+        float verticalAngle = yAccumulator * CalculateMultiple(currentVerticalAngle);//Mathf.Clamp(Camera.main.transform.localEulerAngles.x, 0, 360);
         Camera.main.transform.localEulerAngles = new Vector3(verticalAngle, 0, 0);
+    }
+
+    private float CalculateMultiple(float verticalAngle)
+    {
+        // Mathf.Exp returns e raised to the given power (which is -0.05 * verticalAngle, where
+        // verticalAngle is the X-value of the function f(x) = 1.011233793e^(-0.05x) - 0.011233793f
+        return 1.011233793f * Mathf.Exp(-0.05f * verticalAngle) - 0.011233793f;
     }
 }
 
