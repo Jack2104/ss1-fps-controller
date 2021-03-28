36d489e45a04f45c2f4c237f487a39a07a58e35e
Jack Hill <jackhill2104@gmail.com>
2021-03-28 12:16:35 +1030

    Update PlayerController.cs

--- SS1 FPS Controller/Assets/Scripts/PlayerController.cs
+++ SS1 FPS Controller/Assets/Scripts/PlayerController.cs
@@ -15,22 +15,25 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float currentVerticalAngle = Mathf.Abs(Camera.main.transform.localEulerAngles.x);
+        float currentVerticalAngle = Camera.main.transform.localEulerAngles.x;
 
         float mouseX = Input.GetAxis("Mouse X");
         float mouseY = Input.GetAxis("Mouse Y");
 
+        //print("Current Vertical Angle: "+ currentVerticalAngle.ToString() + ", Multiple: " + CalculateMultiple(currentVerticalAngle).ToString() + ", Mouse Y: " + mouseY.ToString());
+        //print("Current Vertical Angle: " + currentVerticalAngle.ToString() + ", Multiple: " + CalculateMultiple(currentVerticalAngle).ToString() + ", Mouse Y (No multiple): " + (mouseY / CalculateMultiple(currentVerticalAngle)).ToString() + ", Mouse Y (with multiple): " + mouseY.ToString());
+
         // Linearly interpolate from xAccumulator/yAccumulator to mouseX/mouseY
         // Time.deltaTime makes the camera movement frame-rate independent
         xAccumulator = Mathf.Lerp(xAccumulator, mouseX, snappiness * Time.deltaTime);
-        yAccumulator = Mathf.Lerp(yAccumulator, mouseY, snappiness * Time.deltaTime);
+        yAccumulator = Mathf.Lerp(yAccumulator, mouseY, snappiness * Time.deltaTime) * CalculateMultiple(currentVerticalAngle);
 
         transform.Rotate(0, xAccumulator, 0, Space.World);
         Camera.main.transform.Rotate(-yAccumulator, 0, 0);
 
         // Clamp vertical angle between -90 and 90 degrees
-        float verticalAngle = yAccumulator * CalculateMultiple(currentVerticalAngle);//Mathf.Clamp(Camera.main.transform.localEulerAngles.x, 0, 360);
-        Camera.main.transform.localEulerAngles = new Vector3(verticalAngle, 0, 0);
+        //float verticalAngle = Mathf.Clamp(Camera.main.transform.localEulerAngles.x, 0, 360);
+        //Camera.main.transform.localEulerAngles = new Vector3(verticalAngle, 0, 0);
     }
 
     private float CalculateMultiple(float verticalAngle)
