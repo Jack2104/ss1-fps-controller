b9636762a87b4226db6be5b886da5f45024b9fd1
Jack Hill <jackhill2104@gmail.com>
2021-03-28 13:08:36 +1030

    Update PlayerController.cs

--- SS1 FPS Controller/Assets/Scripts/PlayerController.cs
+++ SS1 FPS Controller/Assets/Scripts/PlayerController.cs
@@ -15,29 +15,28 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float currentVerticalAngle = Camera.main.transform.localEulerAngles.x;
-
         float mouseX = Input.GetAxis("Mouse X");
         float mouseY = Input.GetAxis("Mouse Y");
 
-        //print("Current Vertical Angle: "+ currentVerticalAngle.ToString() + ", Multiple: " + CalculateMultiple(currentVerticalAngle).ToString() + ", Mouse Y: " + mouseY.ToString());
-        //print("Current Vertical Angle: " + currentVerticalAngle.ToString() + ", Multiple: " + CalculateMultiple(currentVerticalAngle).ToString() + ", Mouse Y (No multiple): " + (mouseY / CalculateMultiple(currentVerticalAngle)).ToString() + ", Mouse Y (with multiple): " + mouseY.ToString());
+        float currentVerticalAngle = Camera.main.transform.localEulerAngles.x;
+
+        // Convert the angle to a signed angle; change the angle range from [0, 360] to [-180, 180]
+        currentVerticalAngle = (currentVerticalAngle > 180) ? currentVerticalAngle - 360 : currentVerticalAngle;
 
         // Linearly interpolate from xAccumulator/yAccumulator to mouseX/mouseY
         // Time.deltaTime makes the camera movement frame-rate independent
         xAccumulator = Mathf.Lerp(xAccumulator, mouseX, snappiness * Time.deltaTime);
         yAccumulator = Mathf.Lerp(yAccumulator, mouseY, snappiness * Time.deltaTime) * CalculateMultiple(currentVerticalAngle);
 
-        transform.Rotate(0, xAccumulator, 0, Space.World);
-        Camera.main.transform.Rotate(-yAccumulator, 0, 0);
-
-        // Clamp vertical angle between -90 and 90 degrees
-        //float verticalAngle = Mathf.Clamp(Camera.main.transform.localEulerAngles.x, 0, 360);
-        //Camera.main.transform.localEulerAngles = new Vector3(verticalAngle, 0, 0);
+        transform.Rotate(0, xAccumulator, 0, Space.World); // Rotate the player itself
+        Camera.main.transform.Rotate(-yAccumulator, 0, 0); // Rotate the camera
     }
 
     private float CalculateMultiple(float verticalAngle)
     {
+        // Find the absolute value so that negative angles can be inputted (reflect the graph in the Y-axis)
+        verticalAngle = Mathf.Abs(verticalAngle);
+
         // Mathf.Exp returns e raised to the given power (which is -0.05 * verticalAngle, where
         // verticalAngle is the X-value of the function f(x) = 1.011233793e^(-0.05x) - 0.011233793f
         return 1.011233793f * Mathf.Exp(-0.05f * verticalAngle) - 0.011233793f;
